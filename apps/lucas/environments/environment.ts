52509df15baccc0f104ce1413e6714eb4e6cc043
stupidism <sunfeng32@qq.com>
2020-03-29 17:06:29 +0800

    fix(environment): remove useless // prefix

--- apps/lucas/environments/environment.ts
+++ apps/lucas/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   contentful: {
-    host: '//cdn.contentful.com',
+    host: 'cdn.contentful.com',
     accessToken: process.env.NX_CONTENTFUL_PROD_DELIVERY_ACCESS_TOKEN,
     space: 'mbwqgs5je33j',
   },
